49ba6702bbec09cc6791ba5741dd84d239cff764
scarlettp1619 <95775118+scarlettp1619@users.noreply.github.com>
2024-05-04 18:01:14 +0100

    moving stuff to a dev envirnoment for vercel

--- pages/chess/index.tsx
+++
@@ -1,5 +0,0 @@
-export default function Chess(){
-    return(
-        <h1>What is up world!</h1>
-    );
-}
--- pages/chess/scripts/player.tsx
+++
@@ -1,9 +0,0 @@
-import { ChessPiece } from './piece';
-
-export class ChessPlayer {
-    turn: boolean = false;
-    checked: boolean = false;
-    colour: number = 0;
-
-    pieces: ChessPiece[] = [];
-}
