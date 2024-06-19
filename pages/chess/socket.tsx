9d7ced75fbcc947f93464ee946e2c73477817dea
scarlettp1619 <95775118+scarlettp1619@users.noreply.github.com>
2024-06-19 16:26:13 +0100

    eXpOrT dEfAuLt

--- pages/chess/socket.tsx
+++ pages/chess/socket.tsx
@@ -1,6 +1,12 @@
 import { DefaultEventsMap } from '@socket.io/component-emitter';
 import { Socket, io } from 'socket.io-client';
 
+const helper: React.FC = () => {
+  return null;
+};
+
+export default helper;
+
 let socket: Socket<DefaultEventsMap, DefaultEventsMap>;
 const socketInitializer = async () => {
     await fetch('/api/chess/socket');
