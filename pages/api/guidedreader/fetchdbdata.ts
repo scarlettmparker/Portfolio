ab931d122be2e7a2a6763396eb8b2ee643693d07
Scarlett Parker <95775118+scarlettmparker@users.noreply.github.com>
2024-09-30 22:28:18 +0100

    missing prod variables
    nice one there

--- pages/api/guidedreader/fetchdbdata.ts
+++ pages/api/guidedreader/fetchdbdata.ts
@@ -2,18 +2,18 @@ import prisma from '../prismaclient';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    try {
-      const textObjects = await prisma.textObject.findMany({
-        include: {
-            text: {
-                include: {
-                    annotations: true,
-                },
-            },
+  try {
+    const textObjects = await prisma.textObject.findMany({
+      include: {
+        text: {
+          include: {
+            annotations: true,
+          },
         },
-      });
-      res.status(200).json(textObjects);
-    } catch (error) {
-      res.status(500).json({ error: 'Failed to fetch text objects' });
-    }
-  };
+      },
+    });
+    res.status(200).json(textObjects);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch text objects' });
+  }
+};
